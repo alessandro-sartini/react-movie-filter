eadbf71c75d1fa53d9fbb6b6000b20013c71731c
alessandro-sartini <ale.sartini.98@gmail.com>
2025-02-25 11:53:19 +0100

    first commit

---
+++ src/data/arrayFilms.js
@@ -0,0 +1,17 @@
+[
+    { id: 1, title: 'Inception', genre: 'Fantascienza' },
+    { id: 2, title: 'Il Padrino', genre: 'Thriller' },
+    { id: 3, title: 'Titanic', genre: 'Romantico' },
+    { id: 4, title: 'Batman', genre: 'Azione' },
+    { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
+    { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
+    { id: 7, title: 'La La Land', genre: 'Romantico' },
+    { id: 8, title: 'Mad Max: Fury Road', genre: 'Azione' },
+    { id: 9, title: 'The Matrix', genre: 'Fantascienza' },
+    { id: 10, title: 'Fight Club', genre: 'Thriller' },
+    { id: 11, title: 'Forrest Gump', genre: 'Drammatico' },
+    { id: 12, title: 'Il Signore degli Anelli: La Compagnia dell’Anello', genre: 'Fantasy' },
+    { id: 13, title: 'Parasite', genre: 'Thriller' },
+    { id: 14, title: 'Gladiator', genre: 'Azione' },
+    { id: 15, title: 'Amélie', genre: 'Romantico' }
+]
